d4639885ab06cff17555f5e8f3f574fc995a9873
Yogurt <iamskeletonhorn@gmail.com>
2020-09-04 13:50:07 -0700

    Make it so bullets shot by the player don't collide with the player

--- Actors/Characters/Player.gd
+++ Actors/Characters/Player.gd
@@ -16,44 +16,44 @@ var Bullet = preload("res://Actors/Projectiles/Bullet.tscn")
 var next_shoot = 0 # Timestamp of when shooting is possible again
 
 func get_input():
-	get_movement_input()
-	get_shooting_input()
-	
+    get_movement_input()
+    get_shooting_input()
+    
 func get_movement_input():
-	velocity.x = 0
-	var right = Input.is_action_pressed('ui_right')
-	var left = Input.is_action_pressed('ui_left')
-	var jump = Input.is_action_just_pressed('ui_select') || Input.is_action_just_pressed('ui_up')
+    velocity.x = 0
+    var right = Input.is_action_pressed('ui_right')
+    var left = Input.is_action_pressed('ui_left')
+    var jump = Input.is_action_just_pressed('ui_select') || Input.is_action_just_pressed('ui_up')
 
-	if jump and is_on_floor():
-		jumping = true
-		velocity.y = jump_speed
-	if right:
-		velocity.x += run_speed
-	if left:
-		velocity.x -= run_speed
+    if jump and is_on_floor():
+        jumping = true
+        velocity.y = jump_speed
+    if right:
+        velocity.x += run_speed
+    if left:
+        velocity.x -= run_speed
 
 func get_shooting_input():
-	var shooting = Input.is_action_pressed('shoot')
-	
-	if shooting and can_shoot():
-		print("shooting")
-		shoot()
+    var shooting = Input.is_action_pressed('shoot')
+    
+    if shooting and can_shoot():
+        shoot()
+        next_shoot = OS.get_system_time_msecs() + shoot_cooldown
 
 func can_shoot():
-	return OS.get_system_time_msecs() > next_shoot
+    return OS.get_system_time_msecs() > next_shoot
 
 func shoot():
-	var dir = get_global_mouse_position() - global_position
-	var b = Bullet.instance()
-	b.start(position, dir.angle())
-	get_parent().add_child(b)
-	return true
+    var dir = get_global_mouse_position() - global_position
+    var b = Bullet.instance()
+    b.start(position, dir.angle(), true)
+    get_parent().add_child(b)
+    return true
 
 
 func _physics_process(delta):
-	get_input()
-	velocity.y += gravity * delta
-	if jumping and is_on_floor():
-		jumping = false
-	velocity = move_and_slide(velocity, Vector2(0, -1))
+    get_input()
+    velocity.y += gravity * delta
+    if jumping and is_on_floor():
+        jumping = false
+    velocity = move_and_slide(velocity, Vector2(0, -1))
--- Actors/Projectiles/Bullet.gd
+++ Actors/Projectiles/Bullet.gd
@@ -1,26 +1,38 @@
-extends KinematicBody2D
+extends Area2D
 
 export var speed = 250
+
 var velocity = Vector2()
 
-func start(pos, dir):
-	rotation = dir
-	position = pos
-	velocity = Vector2(speed, 0).rotated(rotation)
-	set_rotation(rotation + PI / 2)
+func start(pos, dir, is_player_bullet):
+    rotation = dir
+    position = pos
+    velocity = Vector2(speed, 0).rotated(rotation)
+    set_rotation(rotation + PI / 2)
+    _set_layer(is_player_bullet)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_node("VisibilityNotifier2D").connect("screen_exited", self, "_on_screen_exited")
-	pass # Replace with function body.
+    get_node("VisibilityNotifier2D").connect("screen_exited", self, "_on_screen_exited")
+    connect("body_entered", self, "_on_body_enter")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-func _physics_process(delta):
-	move_and_collide(velocity * delta)
+func _process(delta):
+        position += velocity * delta
 
 func _on_screen_exited():
-	queue_free()
+    queue_free()
+    
+func _on_body_enter(body):
+    print("Body entered the area")
+    print(body)
+    
+    queue_free()
+
+func _set_layer(is_player_bullet):
+    if is_player_bullet:
+        # Only collide with enemy layer
+        set_collision_mask(2)
+    else:
+        # Only collide with player layer
+        set_collision_mask(1)
