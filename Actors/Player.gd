92214230782d9cec43694986e790b043033311ee
Adam <abrostowiczii@gmail.com>
2020-09-04 13:49:38 -0700

    WIP moving platform

--- Actors/Player.gd
+++ Actors/Player.gd
@@ -4,26 +4,31 @@ export (int) var run_speed = 100
 export (int) var jump_speed = -400
 export (int) var gravity = 1200
 
+const FLOOR_NORMAL = Vector2.UP
+
+onready var platform_detector = $PlatformDetector
+
 var velocity = Vector2()
 var jumping = false
 
 func get_input():
-	velocity.x = 0
-	var right = Input.is_action_pressed('ui_right')
-	var left = Input.is_action_pressed('ui_left')
-	var jump = Input.is_action_just_pressed('ui_select') || Input.is_action_just_pressed('ui_up')
+    velocity.x = 0
+    var right = Input.is_action_pressed('ui_right')
+    var left = Input.is_action_pressed('ui_left')
+    var jump = Input.is_action_just_pressed('ui_select') || Input.is_action_just_pressed('ui_up')
 
-	if jump and is_on_floor():
-		jumping = true
-		velocity.y = jump_speed
-	if right:
-		velocity.x += run_speed
-	if left:
-		velocity.x -= run_speed
+    if jump and is_on_floor():
+        jumping = true
+        velocity.y = jump_speed
+    if right:
+        velocity.x += run_speed
+    if left:
+        velocity.x -= run_speed
 
 func _physics_process(delta):
-	get_input()
-	velocity.y += gravity * delta
-	if jumping and is_on_floor():
-		jumping = false
-	velocity = move_and_slide(velocity, Vector2(0, -1))
+    get_input()
+    var is_on_platform = platform_detector.is_colliding()
+    velocity.y += gravity * delta
+    if jumping and is_on_floor():
+        jumping = false
+    velocity = move_and_slide_with_snap(velocity, Vector2(0, -1), FLOOR_NORMAL, not is_on_platform)
