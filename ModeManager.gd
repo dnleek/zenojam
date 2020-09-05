1067c35ce2123fbe94aaf0b87b9ab7cb2876483a
Alexander Biggs <alexanderkbiggs@gmail.com>
2020-09-04 18:42:05 -0700

    Remove my custom level. Remove boilerplate from ModeManager.

--- ModeManager.gd
+++ ModeManager.gd
@@ -1,17 +1,7 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 var is_dark = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-    pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
     if Input.is_action_just_pressed("invert"):
         is_dark = not is_dark
