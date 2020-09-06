1dea8b9428554586d3fba527ab2604cdd4f88ad7
Yogurt <iamskeletonhorn@gmail.com>
2020-09-05 21:07:24 -0700

    Change default behavior of multibullet so the arms are alternating colors if there are an even number. Increased boss health

--- Actors/Projectiles/MultiBullet.gd
+++ Actors/Projectiles/MultiBullet.gd
@@ -8,6 +8,10 @@ export (float) var wait_time = .5
 var Bullet = preload("res://Actors/Projectiles/Bullet.tscn")
 var curr_shot_rotation = 0
 var dark = false
+var alternating_style = ALTERNATE_EVERY_BULLET
+
+# 
+enum {NON_ALTERNATING, ALTERNATE_EVERY_TIMEOUT, ALTERNATE_EVERY_BULLET}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,10 +22,14 @@ func _on_Timer_timeout():
     var angle = (PI * 2) / bullets
     var curr_rot = rotation + curr_shot_rotation
     curr_shot_rotation += shot_rotation
-    dark = not dark
+    
+    if alternating_style == ALTERNATE_EVERY_TIMEOUT:
+        dark = not dark
     
     for i in range(bullets):
         var b = Bullet.instance()
         b.start(position, curr_rot, is_player_bullet, dark)
         get_parent().add_child(b)
         curr_rot += angle
+        if alternating_style == ALTERNATE_EVERY_BULLET:
+            dark = not dark
